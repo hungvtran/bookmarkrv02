18823f87b61e718b5074fddc1fe041e41385175a
hungvtran <hungvtran101@gmail.com>
2015-08-08 16:23:17 -0700

    add before_action filters, and updated show, edit, and upload_file pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user, only: [:edit, :update, :show]
+  
+  def index
+    @users = User.all
+  end
+  
   def show
   	@user = User.find(params[:id])
   end
@@ -57,5 +64,21 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def current_user?(user)
+      user == current_user
     end
 end
